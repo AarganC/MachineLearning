688fbf194a92e45d84dcff004c39a6e1589d041a
AarganC <aargan.cointepas.pro@gmail.com>
2019-07-14 18:06:11 +0000

    Synchro

--- Template/run_model.sh
+++ Template/run_model.sh
@@ -8,9 +8,9 @@ do
    echo "<-------------------------------- $test_name --------------------------------->\n"
    echo "<----------------------------------------------------------------------------->\n"
    nvidia-smi
-   horovodrun -np 4 -H localhost:4 python3 Template_Final.py $test_name $model $bash_size $epoch $activation $layer $nb_filtre
+   horovodrun -np 4 -H localhost:4 python3 main.py $test_name $model $bash_size $epoch $activation $layer $nb_filtre
    sleep 60
    echo "<----------------------------------------------------------------------------->"
    echo "<------------------------------------ FIN ------------------------------------>"
    echo "<----------------------------------------------------------------------------->"
-done < ../HyperParam/file_test.csv
+done < ../Hyperparam/file_test.csv
